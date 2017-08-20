16fa78b6ebb74f29002e63c6a18641c8249e612b
th0rcito <javneira@gmail.com>
2017-08-19 22:30:46 -0300

    .

--- Pizzeriav02/database/migrations/2017_08_20_010116_create_pizzas_table.php
+++ Pizzeriav02/database/migrations/2017_08_20_010116_create_pizzas_table.php
@@ -16,7 +16,12 @@ class CreatePizzasTable extends Migration
         Schema::create('pizzas', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
+            $table->string('name');
+            $table->string('description');
+            $table->float('price',10,2);
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
