bd8cba9b61bca9fddb4a23f1eaee373a1ec8a5c3
th0rcito <javneira@gmail.com>
2017-08-28 23:56:18 -0300

    33 -panel de admin

---
+++ Pizzeriav02/resources/views/admin/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Admin</div>
+
+                <div class="panel-body">
+                    <p>Logueado como admin</p>
+
+                    <hr />
+
+                    <div class="col-md-4">
+                        <div class="panel panel-info">
+                            <div class="panel-heading">
+                                Usuarios
+                            </div>
+                            <div class="panel-body">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="panel panel-info">
+                            <div class="panel-heading">
+                                Pizzas
+                            </div>
+                            <div class="panel-body">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="panel panel-info">
+                            <div class="panel-heading">
+                                Ingredientes
+                            </div>
+                            <div class="panel-body">
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- Pizzeriav02/resources/views/partials/admin-nav.blade.php
+++ Pizzeriav02/resources/views/partials/admin-nav.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-12">
-  {{ link_to_action('Admin\PizzaController@index', 'Pizzas', [], []) }} ||
+  {{-- }}{{ link_to_action('Admin\PizzaController@index', 'Pizzas', [], []) }} ||
   {{ link_to_action('Admin\UserController@index', 'Usuarios', [], []) }} ||
-  {{ link_to_action('Admin\IngredientsController@index', 'Ingredientes', [], []) }} ||
-  {{ link_to_action('Admin\AdminController@index', 'Administracion', [], []) }}
+  {{ link_to_action('Admin\IngredientsController@index', 'Ingredientes', [], []) }} ||--}}
+  {{ link_to_action('AdminController@index', 'Administracion', [], []) }}
 </div>
