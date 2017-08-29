3dba62957d14911b1f64bcbc2e74b138aaf1f3df
th0rcito <javneira@gmail.com>
2017-08-28 23:11:37 -0300

    31 - agregar menu administracion

--- Pizzeriav02/resources/views/partials/nav.blade.php
+++ Pizzeriav02/resources/views/partials/nav.blade.php
@@ -1,6 +1,10 @@
 <p>
   {{link_to_action('PizzaController@create','Crear Pizza',
-    [],[])}}||
+    [],[])}} || 
   {{link_to_action('PizzaController@index','Mis Pizzas',
     [],[])}}
+
+  @if (auth()->user()->role_id === 1)
+    || {{ link_to_route('admin.panel','Administracion',[]) }}
+  @endif
 </p>
