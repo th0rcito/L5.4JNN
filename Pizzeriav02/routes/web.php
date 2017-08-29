2b023095fb8a13299b1dcfa4fb76c3b475ca76e3
th0rcito <javneira@gmail.com>
2017-08-28 22:25:34 -0300

    30 - controller - Administration

--- Pizzeriav02/routes/web.php
+++ Pizzeriav02/routes/web.php
@@ -19,11 +19,15 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware'=>['auth','admin']], function()
+Route::group(['middleware'=>'auth'], function()
 {
   //nombre de ruta pref
   Route::resource('pizzas',"PizzaController");
   //15
   Route::patch('pizzas/{id}/restore','PizzaController@restore')->name('pizzas.restore');
 
+});
+
+Route::group(['middleware'=>'admin','prefix'=>'administration'],function(){
+  Route::get('/','AdminController@index')->name('admin.panel');
 });
